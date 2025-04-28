e067803d1be4f9c04ea7227fe9e43a7b63451bb5
ChristopherPHolder <toholderandrews@gmail.com>
2025-04-28 19:23:23 +0200

    test v18

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,4 +1,10 @@
-import { mergeApplicationConfig, provideAppInitializer, provideEnvironmentInitializer } from '@angular/core';
+import {
+  inject,
+  mergeApplicationConfig,
+  provideAppInitializer,
+  provideEnvironmentInitializer,
+  REQUEST
+} from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { RenderMode, provideServerRouting } from '@angular/ssr';
@@ -6,9 +12,10 @@ import { RenderMode, provideServerRouting } from '@angular/ssr';
 export const serverConfig = mergeApplicationConfig(appConfig, {
   providers: [
     provideServerRendering(),
-    provideServerRouting([{ path: '**', renderMode: RenderMode.Server }]),
+    provideServerRouting([{ path: 'help/**', renderMode: RenderMode.Server }]),
     provideAppInitializer(() => {
-      console.log('Server App Initializer');
+      const req = inject(REQUEST);
+      console.log('Server App Initializer', req);
     }),
     provideEnvironmentInitializer(() => {
       console.log('Server Environment Initializer');
